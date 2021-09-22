80fe6d766337a61a6a458f0a2fdf1a8cf960a966
IluGAN <90762150+xIluGANx@users.noreply.github.com>
2021-09-22 13:19:58 +0300

    Create 4.4.cpp

---
+++ task 4/4.4.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	// ввод переменных
+	cout << "введите a - ";
+	int a;
+	cin >> a;
+	cout << "введиде b - ";
+	int b;
+	cin >> b;
+	//счет
+	// вывод
+	if (a != 0 and b != 0) {
+		cout << "сумма квадратов a и b: " << (a ^ 2) + (b ^ 2) << endl
+			<< "разность квадратов a и b: " << (a ^ 2) - (b ^ 2) << endl
+			<< "произведение квадратов a и b: " << (a ^ 2) * (b ^ 2) << endl
+			<< "частное квадратов a и b: " << (a ^ 2) / (b ^ 2) << "\n";
+	}
+	else {
+		cout << " ОШИБКА : ВВЕДЕН 0";
+	}
+}
