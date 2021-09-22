5fd0f19592c77e374c6d7aec6782f18bb0253093
IluGAN <90762150+xIluGANx@users.noreply.github.com>
2021-09-22 13:01:32 +0300

    Create 4.1.cpp

---
+++ task 4/4.1.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	// ввод переменных
+	int a;
+	std::cout << "введите а - ";
+	std::cin >> a;
+	int b;
+	std::cout << "введите b - ";
+	std::cin >> b;
+	
+	// вычисление периметра
+	int p = 2 * (a + b);
+	// вывод периметра
+	std::cout << p << "\n" ;
+	system("pause");
+
+}
