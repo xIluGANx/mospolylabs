79c3b372d5682c58ec9a99b6581f5b6b51292d72
IluGAN <90762150+xIluGANx@users.noreply.github.com>
2021-09-29 13:21:11 +0300

    Create 5.3.cpp

---
+++ task 5/5.3.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main() {
+    setlocale(LC_ALL, "Russian");
+    // ввод координат точек A B C
+    int Ax, Ay, Bx, By, Cx, Cy;
+    float temp1, temp2;
+    cout << "Координаты точки A. ";
+    cout << "x = ";
+    cin >> Ax;
+    cout << "y = ";
+    cin >> Ay;
+
+    cout << "Координаты точки B. ";
+    cout << "x = ";
+    cin >> Bx;
+    cout << "y = ";
+    cin >> By;
+    cout << "Координаты точки C. ";
+    cout << "x = ";
+    cin >> Cx;
+    cout << "y = ";
+    cin >> Cy;
+    
+    // вычисление
+    temp1 = sqrt(pow(Ax - Bx, 2) + pow(Ay - By, 2));
+    temp2 = sqrt(pow(Bx - Cx, 2) + pow(By - Cy, 2));
+    cout << "Длина отрезка AB: " << temp1 << endl;
+    cout << "Длина отрезка BC: " << temp2 << endl;
+    cout << "Произведение отрезков AC и BC: " << temp1*temp2 << endl;
+
+}
