0710260d8aa30040feb222a1eef014fcb52d3432
IluGAN <90762150+xIluGANx@users.noreply.github.com>
2021-09-29 13:23:28 +0300

    Update 5.1.cpp

--- task 5/5.1.cpp
+++ task 5/5.1.cpp
@@ -4,17 +4,14 @@ using namespace std;
 int main() {
 	setlocale(LC_ALL, "Russian");
 	// ввод переменных x1 y1 x2 y2
+	float x1,y1,x2,y2
 	cout << "введите x1 - ";
-	int x1;
 	cin >> x1;
 	cout << "введите y1 - ";
-	int y1;
 	cin >> y1;
 	cout << "введите x2 - ";
-	int x2;
 	cin >> x2;
 	cout << "введите у2 - ";
-	int y2;
 	cin >> y2;
 	// вычисление расстояния между точками
 	cout << "Расстояние между точками: " << sqrt((pow((x2 - x1), 2)) + (pow((y2 - y1),2))) << "\n";
