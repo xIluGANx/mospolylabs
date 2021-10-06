933babca4331a991f3ba07d23440930d0688a4f8
IluGAN <90762150+xIluGANx@users.noreply.github.com>
2021-10-06 13:42:18 +0300

    Update 6.7.cpp

--- task 6/6.7.cpp
+++ task 6/6.7.cpp
@@ -1,1 +1,15 @@
-
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL,"Russian");
+    //Ввод A и В
+    float x,y,temp,temp2;
+    cout<<"Введите A- ";
+    cin>> x;
+    //выполнение задачи
+    temp=pow(x,11);
+    y=temp*x*x*x*x;
+    cout<<"Ans: "<<y;
+}
