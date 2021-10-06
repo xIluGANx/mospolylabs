dabe7fb79ab98d960efb4f2f36a10f93ac58d9cc
IluGAN <90762150+xIluGANx@users.noreply.github.com>
2021-10-06 13:19:12 +0300

    Update 6.2.cpp

--- task 6/6.2.cpp
+++ task 6/6.2.cpp
@@ -5,7 +5,7 @@ int main()
 {
     setlocale(LC_ALL,"Russian");
     //Ввод A и В
-    float A,B,C,c;
+    float A,B,C,a,b;
     cout<<"Введите A- ";
     cin>> A;
     cout<<"Введите B- ";
@@ -14,15 +14,10 @@ int main()
     cin>>C;
         
     //выполнение задачи
-    c=A;
-    A=B;
-    B=c;
-    c=B;
-    B=C;
-    C=c;
-    c=C;
-    C=A;
-    A=c;
+    a=C;C=B;B=a;
+    a=A;A=B;B=a;
+    
+    
     cout<<"A="<<A<<endl;
     cout<<"B="<<B<<endl;
     cout<<"C="<<C;
