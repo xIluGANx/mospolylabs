062dd03b13160aa6d1c81111d596442509411fdd
IluGAN <90762150+xIluGANx@users.noreply.github.com>
2021-10-06 12:51:58 +0300

    Update 5.5.cpp

--- task 5/5.5.cpp
+++ task 5/5.5.cpp
@@ -23,10 +23,15 @@ int main() {
     cin >> Cy;
     
     // вычисление
+    if (x1!=x2&x2!=x3&x1!=x3&y1!=y2&y2!=y3&y1!=y3){
     temp1 = sqrt(pow(Ax - Bx, 2) + pow(Ay - By, 2));
     temp2 = sqrt(pow(Bx - Cx, 2) + pow(By - Cy, 2));
     temp3 = sqrt(pow(Cx - Ax, 2) + pow(Cy - Ay, 2));
     p = (temp1 + temp2 + temp3) / 2;
     cout << "Периметр треугольника: " << temp1 + temp2 + temp3;
     cout << "Площадь треугольника: " << sqrt(p * (p - temp1) * (p - temp2) * (p - temp3));
-}
+    }
+    else{
+        cout<<"Ошибка. Некоторые из точек лежат на одной прямой"
+    }
+    }
