0a09c8cca8fce325616425f2664e17156395f5fd
IluGAN <90762150+xIluGANx@users.noreply.github.com>
2022-03-06 14:34:22 +0300

    Create shablon_multiprog.cpp

---
+++ OOP_LABS/shablon_multiprog.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <iomanip>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	bool End = true;
+	int num;
+	//switch cases
+	while (End)
+	{
+		cout << "Choose number of task(0 if u want to exit):";
+		cin >> num;
+		switch (num)
+		{
+		case 1:
+		case 2:
+		case 3:
+		case 4:
+		case 5:
+		case 6:
+		case 7:
+		case 8:
+    case 9:
+		case 10:
+		case 11:
+		case 12:
+		default:
+			End = false;
+			break;
+		}
+	}
+	return 0;
+}
