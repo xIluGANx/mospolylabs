193fcd7d5c399c40f2bda2400bb440a94b3a1b40
IluGAN <90762150+xIluGANx@users.noreply.github.com>
2022-03-09 18:57:46 +0300

    Update 4.cpp

--- OOP_LABS/4.cpp
+++ OOP_LABS/4.cpp
@@ -2,23 +2,47 @@
 #include <iomanip>
 #include <math.h>
 using namespace std;
-//void 5
+// 5
 long hms_to_secs(int h, int m, long s);
-//void 6
+// 6
 long time_to_secs(int h, int m, long s);
 int secs_to_time(int h, int m, long s);
 //struct 6
 struct timee {
 	int seconds, minutes, hours;
 };
-//void 7
+// 7
 double power(double n, int p = 2);
 double power(char n, int p = 2);
 double power(int n, int p = 2);
 double power(long n, int p = 2);
 double power(float n, int p = 2);
-//enum 7
-enum type{dou,cha,in,lon,floa};
+//7
+enum type { dou, cha, in, lon, floa };
+//8
+void swap(int* a, int* b);
+// 10
+void foo();
+//struct 11
+	struct sterling {
+		int pounds, shillings, pence;
+	};
+// 11
+	sterling inputsterling(sterling a);
+	sterling count(sterling a,sterling b,sterling sum);
+	sterling output(sterling a);
+	sterling a, b, sum;
+//struct12
+	struct fraction {
+		int numerator;
+		int denominator;
+	};
+//12
+	fraction fadd(fraction a1, fraction b1);
+	fraction fsub(fraction a1, fraction b1);
+	fraction fmul(fraction a1 , fraction b1);
+	fraction fdiv(fraction a1, fraction b1);
+	fraction a1,b1,summ;
 int main()
 {
 	setlocale(LC_ALL, "ru");
@@ -62,23 +86,23 @@ int main()
 			bool kon = true;
 			while (kon)
 			{
-			int a, b,l;
-			string yn;
-			long c;
-			cout << "Введите часы: ";
-			cin >> a;
-			cout << "Введите минуты: ";
-			cin >> b;
-			cout << "Введите секунды: ";
-			cin >> c;
-			c=hms_to_secs(a, b, c);
-			cout << "всего секунд : " << c << endl;
-			cout << "Продолжить? y/n: ";
-			cin >> yn;
-			if (yn == "n")
-			{
-				kon = false;
-			}
+				int a, b, l;
+				string yn;
+				long c;
+				cout << "Введите часы: ";
+				cin >> a;
+				cout << "Введите минуты: ";
+				cin >> b;
+				cout << "Введите секунды: ";
+				cin >> c;
+				c = hms_to_secs(a, b, c);
+				cout << "всего секунд : " << c << endl;
+				cout << "Продолжить? y/n: ";
+				cin >> yn;
+				if (yn == "n")
+				{
+					kon = false;
+				}
 
 			}
 			break;
@@ -100,12 +124,12 @@ int main()
 				cin >> t1.minutes;
 				cout << "Введите секунды: ";
 				cin >> t1.seconds;
-				c =(long)t1.seconds;
-				time_to_secs(t1.hours, t1.minutes , c);
+				c = (long)t1.seconds;
+				time_to_secs(t1.hours, t1.minutes, c);
 				cout << "всего секунд : " << c << endl;
 				break;
 			}
-			case 2: 
+			case 2:
 			{
 				cout << "Введите секунды: ";
 				cin >> c;
@@ -121,7 +145,7 @@ int main()
 			}
 			break;
 		}
-		}
+		
 		case 7:
 			// // // // // // // // // // // //
 		{
@@ -132,153 +156,208 @@ int main()
 			type nom;
 			cout << "какого типа переменная? первая буква: ";
 			cin >> m;
-				switch (m) {
-				case 'd':
-					nom = dou;
-				case 'c':
-					nom = cha;
-				case 'i':
-					nom = in;
-				case 'l':
-					nom = lon;
-				case 'f':
-					nom = floa;
-				default:
-					break;
-				};
-				switch (nom)
-				{
-				case dou:{
-					double number;
-					cout << "введите число:";//get number
-					cin >> number;
-					cout << "будете вводить степень (y/n)? ";
-					cin >> yeserno;
-					if (yeserno == 'y')//пользователю нужен не квадрат числа?
-					{
-						cout << "ведите степень: ";
-						cin >> pow;
-						answer = power(number, pow);//возвести число в степень 
-					}
-					else answer = power(number);//квадрат числа   
-
-					cout << "ответ " << answer << endl;
-
-					break; }
-
-				case cha: {
-					char number;
-					cout << "введите число:";//get number
-					cin >> number;
-					cout << "будете вводить степень (y/n)? ";
-					cin >> yeserno;
-					if (yeserno == 'y')//пользователю нужен не квадрат числа?
-					{
-						cout << "ведите степень: ";
-						cin >> pow;
-						answer = power(number, pow);//возвести число в степень 
-					}
-					else answer = power(number);//квадрат числа   
-
-					cout << "ответ " << answer << endl;
-
-					break; }
-				case in:
-				{
-					int number;
-					cout << "введите число:";//get number
-					cin >> number;
-					cout << "будете вводить степень (y/n)? ";
-					cin >> yeserno;
-					if (yeserno == 'y')//пользователю нужен не квадрат числа?
-					{
-						cout << "ведите степень: ";
-						cin >> pow;
-						answer = power(number, pow);//возвести число в степень 
-					}
-					else answer = power(number);//квадрат числа   
-
-					cout << "ответ " << answer << endl;
-
-					break; }
-
-				case lon:
-				{
-					long number;
-					cout << "введите число:";//get number
-					cin >> number;
-					cout << "будете вводить степень (y/n)? ";
-					cin >> yeserno;
-					if (yeserno == 'y')//пользователю нужен не квадрат числа?
-					{
-						cout << "ведите степень: ";
-						cin >> pow;
-						answer = power(number, pow);//возвести число в степень 
-					}
-					else answer = power(number);//квадрат числа   
-
-					cout << "ответ " << answer << endl;
-
-					break; }
-				case floa:
-				{
-					double float;
-					cout << "введите число:";//get number
-					cin >> number;
-					cout << "будете вводить степень (y/n)? ";
-					cin >> yeserno;
-					if (yeserno == 'y')//пользователю нужен не квадрат числа?
-					{
-						cout << "ведите степень: ";
-						cin >> pow;
-						answer = power(number, pow);//возвести число в степень 
-					}
-					else answer = power(number);//квадрат числа   
-
-					cout << "ответ " << answer << endl;
-
-					break; }
-				default:
-					break;
-				}
+			switch (m) {
+			case 'd':
+				nom = dou;
+			case 'c':
+				nom = cha;
+			case 'i':
+				nom = in;
+			case 'l':
+				nom = lon;
+			case 'f':
+				nom = floa;
+			default:
 				break;
+			};
+			switch (nom)
+			{
+			case dou: {
+				double number;
+				cout << "введите число:";//get number
+				cin >> number;
+				cout << "будете вводить степень (y/n)? ";
+				cin >> yeserno;
+				if (yeserno == 'y')//пользователю нужен не квадрат числа?
+				{
+					cout << "ведите степень: ";
+					cin >> pow;
+					answer = power(number, pow);//возвести число в степень 
+				}
+				else answer = power(number);//квадрат числа   
+
+				cout << "ответ " << answer << endl;
+
+				break; }
+
+			case cha: {
+				char number;
+				cout << "введите число:";//get number
+				cin >> number;
+				cout << "будете вводить степень (y/n)? ";
+				cin >> yeserno;
+				if (yeserno == 'y')//пользователю нужен не квадрат числа?
+				{
+					cout << "ведите степень: ";
+					cin >> pow;
+					answer = power(number, pow);//возвести число в степень 
+				}
+				else answer = power(number);//квадрат числа   
+
+				cout << "ответ " << answer << endl;
+
+				break; }
+			case in:
+			{
+				int number;
+				cout << "введите число:";//get number
+				cin >> number;
+				cout << "будете вводить степень (y/n)? ";
+				cin >> yeserno;
+				if (yeserno == 'y')//пользователю нужен не квадрат числа?
+				{
+					cout << "ведите степень: ";
+					cin >> pow;
+					answer = power(number, pow);//возвести число в степень 
+				}
+				else answer = power(number);//квадрат числа   
+
+				cout << "ответ " << answer << endl;
+
+				break; }
+
+			case lon:
+			{
+				long number;
+				cout << "введите число:";//get number
+				cin >> number;
+				cout << "будете вводить степень (y/n)? ";
+				cin >> yeserno;
+				if (yeserno == 'y')//пользователю нужен не квадрат числа?
+				{
+					cout << "ведите степень: ";
+					cin >> pow;
+					answer = power(number, pow);//возвести число в степень 
+				}
+				else answer = power(number);//квадрат числа   
+
+				cout << "ответ " << answer << endl;
+
+				break; }
+			case floa:
+			{
+				float number;
+				cout << "введите число:";//get number
+				cin >> number;
+				cout << "будете вводить степень (y/n)? ";
+				cin >> yeserno;
+				if (yeserno == 'y')//пользователю нужен не квадрат числа?
+				{
+					cout << "ведите степень: ";
+					cin >> pow;
+					answer = power(number, pow);//возвести число в степень 
+				}
+				else answer = power(number);//квадрат числа   
+
+				cout << "ответ " << answer << endl;
+
+				break; }
+			default:
+				break;
+			}
+			break;
 		}
 		case 8:
 			// // // // // // // // // // // //
 		{
-			cout << " choose only 5 or further";
+			int a, b;
+			cout << "введите 2 числа и мы их поменяем местами!" << endl;
+			cin >> a >> b;
+			swap(a, b);
+			cout << a << " " << b << endl;
 			break;
 		}
 		case 9:
 			// // // // // // // // // // // //
 		{
-			cout << " choose only 5 or further";
+			timee t1;
+			cout << "введите 2 числа: минуты и секунды. И мы их поменяем местами!" << endl;
+			cin >> t1.minutes >> t1.seconds;
+			swap(t1.minutes, t1.seconds);
+			cout << t1.minutes<< " " << t1.seconds << endl;
 			break;
+			
 		}
 		case 10:
 			// // // // // // // // // // // //
 		{
-			cout << " choose only 5 or further";
-			break;
+			
+			int a;
+			cout << "сколько раз вызвать функцию? ";
+			cin >> a;
+			while (a != 0) {
+				foo();
+				a--;
+			}
 		}
 		case 11:
 			// // // // // // // // // // // //
 		{
-			cout << " choose only 5 or further";
-			break;
+			
+			a = inputsterling(a);
+			b = inputsterling(b);
+			sum = count(a, b, sum);
+			output(sum);
 		}
 		case 12:
 			// // // // // // // // // // // //
 		{
-			cout << " choose only 5 or further";
+			bool kon = true;
+			while (kon)
+			{
+				char oper;
+				string yn;
+				cout << "Введите операцию ";
+				cin >> a1.numerator >> a1.denominator >> oper >> b1.numerator >> b1.denominator;
+				switch (oper) {
+				case '+':
+				{
+					summ = fadd(a1, b1);
+					break;
+				}
+				case '-':
+				{
+					summ = fsub(a1, b1);
+					break;
+				}
+				case '*':
+				{
+					summ = fmul(a1, b1);
+					break;
+				}
+				case '/':
+				{
+					summ = fdiv(a1, b1);
+					break;
+				}
+				default: {cout << "Answer = none"; break; }
+				}
+				cout << "Answer = " << summ.numerator << "/" << summ.denominator << endl;
+				cout << "Продолжить? y/n: ";
+				cin >> yn;
+				if (yn == "n")
+				{
+					kon = false;
+				}
+			}
 			break;
 		}
 		default:
 			End = false;
 			break;
-		}
 	}
-	return 0;
+}
+return 0;
 }
 //5 ------------------------------------
 long hms_to_secs(int h, int m, long s)
@@ -306,8 +385,8 @@ int secs_to_time(int h, int m, long s)
 double power(double n, int p)
 {
 	double result = 1.0;//начать с 1                      
-	for(int j=0;j<p;j++)//умножить на n                 
-	result *=n;//p раз                    
+	for (int j = 0; j < p; j++)//умножить на n                 
+		result *= n;//p раз                    
 	return result;
 }
 double power(char n, int p)
@@ -338,3 +417,74 @@ double power(float n, int p)
 		result *= n;//p раз                    
 	return result;
 }
+//----------------------------------
+void swap(int* a, int* b)
+{
+	int temp = *a;
+	*a = *b;
+	* b = temp;
+}
+
+void foo()
+{
+	static int x = 0;
+	x++;
+	cout << "вызвано " << x << " раз" << endl;
+}
+//11 ------------------------------------------
+sterling inputsterling(sterling a)
+{
+	cout << "Введите фунты: ";
+	cin >> a.pounds;
+	cout << "Введите шиллинги: ";
+	cin >> a.shillings;
+	cout << "Введите пенсы:";
+	cin >> a.pence;
+	return a;
+}
+
+sterling count(sterling a, sterling b,sterling sum)
+{
+	int summ = (a.pounds * 19*11 + a.shillings * 11 + a.pence) + (b.pounds * 19*11 + b.shillings * 11 + b.pence);
+	sum.pounds = summ / (19 * 11);
+	sum.shillings = summ % (19 * 11) / 11;
+	sum.pence = summ % (19 * 11) % 11;
+	return sum;
+}
+
+sterling output(sterling a)
+{
+	cout << "Сумма: " << a.pounds << " " << a.shillings << " " << a.pence << endl;
+	return a;
+}
+//12 -------------------------------------------------------------------------------
+
+fraction fadd(fraction a1, fraction b1)
+{
+	summ.numerator = a1.numerator * b1.denominator + a1.denominator * b1.numerator;
+	summ.denominator = a1.denominator * b1.denominator;
+	return summ;
+
+}
+
+fraction fsub(fraction a1, fraction b1)
+{
+	summ.numerator = a1.numerator * b1.denominator - a1.denominator * b1.numerator;
+	summ.denominator = a1.denominator * b1.denominator;
+	return summ;
+}
+
+fraction fmul(fraction a1, fraction b1)
+{
+	summ.numerator = a1.numerator * b1.denominator;
+	summ.denominator = a1.denominator * b1.denominator;
+	return summ;
+}
+
+fraction fdiv(fraction a1, fraction b1)
+{
+	summ.numerator = a1.numerator * b1.denominator;
+	summ.denominator = a1.denominator * b1.numerator;
+	return summ;
+}
+
